d395b52b9668b1989e129f98fd17ea4a567efc10
thanhvietgl <thanhvietgl1210@gmail.com>
2022-05-08 23:12:53 +0700

    1st demo design protocol

---
+++ packages/react-app/src/pages/Protocol/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Swap } from '../../components/Swap';
+import { addresses } from '@dex/contracts';
+import { useEthers } from '@usedapp/core';
+import { Protocol } from '../../components/Protocol';
+
+const Protocols = () => {
+  const { active } = useEthers();
+  const { one, two } = addresses['4'];
+  const [swapReversed, setSwapReversed] = useState(false);
+  const swapPositionBtn = (ev) => {
+    ev.preventDefault();
+    setSwapReversed((prv) => !prv);
+  };
+
+  return (
+    <div>
+      {active && (
+        <>
+          {swapReversed ? (
+            <Protocol token0Address={two} token1Address={one} swapPosition={swapPositionBtn} />
+          ) : (
+            <Protocol token0Address={one} token1Address={two} swapPosition={swapPositionBtn} />
+          )}
+          <hr />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Protocols;
